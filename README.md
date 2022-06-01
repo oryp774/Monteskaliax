81d2a4be89febbcb338e3e4bb6689d03c3e025b3
oryp774 <73075287+oryp774@users.noreply.github.com>
2022-06-01 23:17:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Monteskaliax
+le bot
---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+client.commands = new Collection();
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'beep') {
+        await interaction.reply('Boop!');
+    }
+});
+
+client.login(token);
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const command = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
+
+// Raw data that can be used to register a slash command
+const rawData = command.toJSON();
